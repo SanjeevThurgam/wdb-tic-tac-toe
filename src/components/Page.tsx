435a00788f2974934d2b76204515b7c80b39ddb1
william-b-henderson <william_henderson@berkeley.edu>
2022-03-02 15:46:06 -0800

    Add state for Page component

---
+++ src/components/Page.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { ChakraProvider, Flex, theme } from '@chakra-ui/react';
+// import { ColorModeSwitcher } from './ColorModeSwitcher';
+
+export const Page = () => {
+
+    const [playerScore, setPlayerScore] = useState([0,0]);
+    const [currentPlayer, setCurrentPlayer] = useState(0);
+    //const [tileState, setTileState] = useState([0,0,0,0,0,0,0,0,0,])
+
+    return (
+        <ChakraProvider theme={theme}>
+            <Flex w="100%" h="100%" bg="gray" direction="column" >
+                Box
+            </Flex>
+        </ChakraProvider>
+    )
+};
+
+export default Page;
