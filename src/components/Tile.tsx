43635e729f05fc6db7ab2dca38e5eb222042293d
william-b-henderson <william_henderson@berkeley.edu>
2022-03-02 19:08:21 -0800

    Resize O and X Icons to better fit in boxes

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -3,26 +3,27 @@ import CircleIcon from './CircleIcon';
 import XIcon from './XIcon'
 
 
-import { ChakraProvider, Box, theme } from '@chakra-ui/react';
+import { ChakraProvider, Flex, theme } from '@chakra-ui/react';
 // import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 
 const Tile = ({player}: {player: number}): JSX.Element => {
 
-    const displayIcon = (player === 0) ? <CircleIcon boxSize="100%"/> : <XIcon boxSize="100%"/>
+    const displayIcon = (player === 0) ? <CircleIcon w="100%" h="90%"/> : <XIcon w="100%" h="90%"/>
 
     return (
         <ChakraProvider theme={theme}>
-            <Box 
+            <Flex 
                 w="100%" 
                 h="100%" 
                 bg="#626887" 
-                alignContent="center"
+                align="center"
+                justify="center"
                 borderRadius="xl">
                 {/* <Center> */}
                     {(player === 2) ? <> </> : displayIcon}
                 {/* </Center> */}
-            </Box>
+            </Flex>
         </ChakraProvider>
     )
 };
